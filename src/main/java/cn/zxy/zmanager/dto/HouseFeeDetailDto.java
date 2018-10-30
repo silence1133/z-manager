b3d792ac9f4a60a476e75993a24684e8f4338cf4
cnbo <18163912002@163.com>
2018-10-30 23:26:03 +0800

    add comment

--- src/main/java/cn/zxy/zmanager/dto/HouseFeeDetailDto.java
+++ src/main/java/cn/zxy/zmanager/dto/HouseFeeDetailDto.java
@@ -7,14 +7,23 @@ import lombok.Data;
 @Data
 public class HouseFeeDetailDto {
 	
+	// 合同 ID
 	private Integer contractId;
+	// 应缴截止日期
 	private Date payDeadline;
+	// 第几年
 	private Integer sortYear;
+	// 第 N 年的总租金
 	private Integer totalRentFee;
+	// 第 N 年的总物业费
 	private Integer totalPropertyFee;
+	// 第 N 年已缴租金
 	private Integer paidRentFee;
+	// 第 N 年已缴物业费
 	private Integer paidPropertyFee;
+	// 第 N 年剩余应缴租金
 	private Integer restRentFee;
+	// 第 N 年剩余应缴物业费
 	private Integer restPropertyFee;
 
 }
