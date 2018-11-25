fe6479ecab183d3b05f789e4f55b8c0eed2b3106
cnbo <18163912002@163.com>
2018-11-25 23:33:18 +0800

    update

--- src/main/java/cn/zxy/zmanager/service/impl/ZElectricRecordAddServiceImpl.java
+++ src/main/java/cn/zxy/zmanager/service/impl/ZElectricRecordAddServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.zxy.zmanager.service.impl;
 
 import java.util.Calendar;
+import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import cn.zxy.zmanager.dao.mapper.ZElectricRecordMapper;
 import cn.zxy.zmanager.dto.ElectricRecordDTO;
 import cn.zxy.zmanager.service.ZElectricRecordAddService;
 import cn.zxy.zmanager.support.LoginUser;
+import cn.zxy.zmanager.support.common.ResultCode;
 import cn.zxy.zmanager.support.common.ZManagerResult;
 import cn.zxy.zmanager.support.common.utils.DateUtils;
 import lombok.Data;
@@ -25,26 +27,36 @@ public class ZElectricRecordAddServiceImpl implements ZElectricRecordAddService 
 
 	@Autowired
 	private ZContractMapper contractMapper;
-	
+
 	@Autowired
 	private ZElectricMeterMapper electricMeterMapper;
-	
+
 	@Autowired
 	private ZElectricRecordMapper electricRecordMapper;
-	
+
 	@Transactional
 	@Override
 	public ZManagerResult<?> addElectricRecord(ElectricRecordDTO electricRecordDTO, LoginUser loginUser) {
-		ZElectricMeter electricMeterFromDB = electricMeterMapper.selectByPrimaryKey(electricRecordDTO.getElectricMeterId());
+		ZElectricMeter electricMeterFromDB = electricMeterMapper
+				.selectByPrimaryKey(electricRecordDTO.getElectricMeterId());
+		if (isIllegalMark(electricRecordDTO, electricMeterFromDB)) {
+			return ZManagerResult.fail(ResultCode.FAILURE.getCode(), "您录入的电表刻度有误，电表刻度不能小于当前系统的终止刻度，新增失败！");
+		}
 		ZElectricRecord electricRecord = getElectricRecord(electricMeterFromDB, electricRecordDTO, loginUser);
 		ZElectricMeter newElectricMeter = getNewElectricMeter(electricMeterFromDB, electricRecord, loginUser);
 		ZContract newContract = getNewContract(newElectricMeter, loginUser);
-		
+
 		electricMeterMapper.updateByPrimaryKeySelective(newElectricMeter);
 		electricRecordMapper.insertSelective(electricRecord);
 		contractMapper.updateByPrimaryKeySelective(newContract);
-		
+
 		return ZManagerResult.success();
+	}
+
+	private boolean isIllegalMark(ElectricRecordDTO electricRecordDTO, ZElectricMeter electricMeterFromDB) {
+		int oldMark = Optional.ofNullable(electricMeterFromDB.getTotalUseElectric()).orElse(0)
+				+ Optional.ofNullable(electricMeterFromDB.getInitMark()).orElse(0);
+		return electricRecordDTO.getCurrentMark() < oldMark;
 	}
 
 	private ZContract getNewContract(ZElectricMeter newElectricMeter, LoginUser loginUser) {
@@ -55,7 +67,7 @@ public class ZElectricRecordAddServiceImpl implements ZElectricRecordAddService 
 		e.setModifyTime(DateUtils.getCurrentDate());
 		e.setModifyEmp(loginUser.getName());
 		e.setModifyEmpId(loginUser.getId());
-		
+
 		return e;
 	}
 
@@ -70,7 +82,7 @@ public class ZElectricRecordAddServiceImpl implements ZElectricRecordAddService 
 		e.setModifyEmp(loginUser.getName());
 		e.setModifyEmpId(loginUser.getId());
 		e.setModifyTime(DateUtils.getCurrentDate());
-		
+
 		return e;
 	}
 
@@ -87,7 +99,7 @@ public class ZElectricRecordAddServiceImpl implements ZElectricRecordAddService 
 		record.setCreateEmp(loginUser.getName());
 		record.setCreateEmpId(loginUser.getId());
 		record.setCreateTime(DateUtils.getCurrentDate());
-		
+
 		return record;
 	}
 
--- src/main/java/cn/zxy/zmanager/service/impl/ZWaterRecordAddServiceImpl.java
+++ src/main/java/cn/zxy/zmanager/service/impl/ZWaterRecordAddServiceImpl.java
@@ -2,6 +2,7 @@ package cn.zxy.zmanager.service.impl;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,12 +17,13 @@ import cn.zxy.zmanager.dao.mapper.ZWaterRecordMapper;
 import cn.zxy.zmanager.dto.WaterRecordDTO;
 import cn.zxy.zmanager.service.ZWaterRecordAddService;
 import cn.zxy.zmanager.support.LoginUser;
+import cn.zxy.zmanager.support.common.ResultCode;
 import cn.zxy.zmanager.support.common.ZManagerResult;
 import cn.zxy.zmanager.support.common.utils.DateUtils;
 
 @Service
 public class ZWaterRecordAddServiceImpl implements ZWaterRecordAddService {
-	
+
 	@Autowired
 	private ZWaterRecordMapper waterRecordMapper;
 
@@ -31,22 +33,30 @@ public class ZWaterRecordAddServiceImpl implements ZWaterRecordAddService {
 	@Autowired
 	private ZContractMapper contractMapper;
 
-
 	@Transactional
 	@Override
 	public ZManagerResult<?> addWaterRecord(WaterRecordDTO waterRecordDTO, LoginUser loginUser) {
+		if (isIllegalMark(waterRecordDTO)) {
+			return ZManagerResult.fail(ResultCode.FAILURE.getCode(), "您录入的水表刻度有误，水表刻度不能小于当前系统的终止刻度，新增失败！");
+		}
 		ZWaterMeter waterMeterFromDB = waterMeterMapper.selectByPrimaryKey(waterRecordDTO.getWaterMeterId());
 		ZWaterRecord waterRecord = getWaterRecrod(waterRecordDTO, waterMeterFromDB, loginUser);
 		ZWaterMeter newWaterMeter = getNewWaterMeter(waterMeterFromDB, waterRecord, loginUser);
 		ZContract newContract = getNewContract(newWaterMeter, loginUser);
-		
+
 		waterMeterMapper.updateByPrimaryKeySelective(newWaterMeter);
 		waterRecordMapper.insertSelective(waterRecord);
 		contractMapper.updateByPrimaryKeySelective(newContract);
-		
+
 		return ZManagerResult.success();
 	}
 
+	private boolean isIllegalMark(WaterRecordDTO waterRecordDTO) {
+		ZWaterMeter waterMeter = waterMeterMapper.selectByPrimaryKey(waterRecordDTO.getWaterMeterId());
+		int oldMark = Optional.ofNullable(waterMeter.getTotalWater()).orElse(0)
+				+ Optional.ofNullable(waterMeter.getInitMark()).orElse(0);
+		return waterRecordDTO.getCurrentMark() < oldMark;
+	}
 
 	private ZContract getNewContract(ZWaterMeter newWaterMeter, LoginUser loginUser) {
 		ZContract contractFromDB = contractMapper.selectByPrimaryKey(newWaterMeter.getContractId());
@@ -55,10 +65,9 @@ public class ZWaterRecordAddServiceImpl implements ZWaterRecordAddService {
 		e.setModifyEmp(loginUser.getName());
 		e.setModifyEmpId(loginUser.getId());
 		e.setModifyTime(DateUtils.getCurrentDate());
-		
+
 		return e;
 	}
-
 
 	private ZWaterMeter getNewWaterMeter(ZWaterMeter waterMeterFromDB, ZWaterRecord waterRecord, LoginUser loginUser) {
 		ZWaterMeter e = ZWaterRecord.genWaterMeter(waterRecord);
@@ -69,15 +78,15 @@ public class ZWaterRecordAddServiceImpl implements ZWaterRecordAddService {
 		e.setModifyTime(DateUtils.getCurrentDate());
 		e.setModifyEmp(loginUser.getName());
 		e.setModifyEmpId(loginUser.getId());
-		
+
 		return e;
 	}
 
-
-	private ZWaterRecord getWaterRecrod(WaterRecordDTO waterRecordDTO, ZWaterMeter waterMeterFromDB, LoginUser loginUser) {
+	private ZWaterRecord getWaterRecrod(WaterRecordDTO waterRecordDTO, ZWaterMeter waterMeterFromDB,
+			LoginUser loginUser) {
 		ZWaterRecord record = ZWaterMeter.genWaterMeterRecord(waterMeterFromDB);
-		
-		record.setEndMark((int)(Math.floor(Double.valueOf(waterRecordDTO.getCurrentMark()))));
+
+		record.setEndMark((int) (Math.floor(Double.valueOf(waterRecordDTO.getCurrentMark()))));
 		Calendar c = Calendar.getInstance();
 		c.setTime(waterRecordDTO.getMarkDate());
 		record.setMarkDate(waterRecordDTO.getMarkDate());
@@ -87,7 +96,7 @@ public class ZWaterRecordAddServiceImpl implements ZWaterRecordAddService {
 		record.setCreateEmp(loginUser.getName());
 		record.setCreateEmpId(loginUser.getId());
 		record.setCreateTime(DateUtils.getCurrentDate());
-		
+
 		return record;
 	}
 
